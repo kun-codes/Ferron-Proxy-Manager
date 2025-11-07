33a15efcde2008168f31bbc261b219026e6b5778
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-11-07 14:14:01 +0530

    feat(ferron): implement `FerronConfig` class for config parsing

---
+++ backend/src/ferron/service.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+
+import ckdl
+
+class FerronConfig:
+    def __init__(self, config_file: Path):
+        self.config_file = config_file
+
+        with open(self.config_file, "r", encoding="utf-8") as f:
+            text = f.read()
+
+
+        self.parsed_config = ckdl.parse(text, version=2)
+
+    def find_node(self, node_name: str, is_snippet: bool = False) -> ckdl.Node | None:
+        nodes = self.parsed_config.nodes
+
+        for node in nodes:
+            if is_snippet:
+                if node.name != "snippet":
+                    continue
+
+                if node.args:
+                    first_arg = node.args[0]
+                    if first_arg == node_name:
+                        return node
+
+            else:
+                if node.name == node_name:
+                    return node
+
+        return None
