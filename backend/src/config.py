14d67ef844e9c8966b88734dd685eb8e174d6349
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-12-14 13:44:18 +0530

    feat(backend docker): add Dockerfile and docker-compose for development and production

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,21 +1,17 @@
-from pathlib import Path
-
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    # for application
     app_name: str = "Ferron Proxy Manager"
     app_version: str = "0.1.0"
     debug: bool
     
-    # for database
     database_url: str
     database_echo: bool
     
     model_config = SettingsConfigDict(
-        env_file=str(Path(__file__).resolve().parents[1] / ".env"),
-        extra="ignore"
+        extra="ignore",
+        case_sensitive=False,
     )
 
 
