f3cd761ca77e2cfaa5d55cba859ad4890892e758
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-12-15 02:15:01 +0530

    feat(ferron global config): implement CRUD operations for global configuration to db

---
+++ backend/src/ferron/schemas.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel, Field
+
+from src.ferron.constants import (
+    DEFAULT_HTTP_PORT,
+    DEFAULT_HTTPS_PORT,
+    DEFAULT_IS_H1_PROTOCOL_ENABLED,
+    DEFAULT_IS_H2_PROTOCOL_ENABLED,
+    DEFAULT_IS_H3_PROTOCOL_ENABLED,
+    DEFAULT_TIMEOUT,
+    DEFAULT_CACHE_MAX_ENTRIES,
+)
+
+class GlobalConfig(BaseModel):
+    default_http_port: int = Field(default=DEFAULT_HTTP_PORT, ge=1, le=65535)
+    default_https_port: int = Field(default=DEFAULT_HTTPS_PORT, ge=1, le=65535)
+    is_h1_protocol_enabled: bool = DEFAULT_IS_H1_PROTOCOL_ENABLED
+    is_h2_protocol_enabled: bool = DEFAULT_IS_H2_PROTOCOL_ENABLED
+    is_h3_protocol_enabled: bool = DEFAULT_IS_H3_PROTOCOL_ENABLED
+    timeout: int = DEFAULT_TIMEOUT
+    cache_max_entries: int = DEFAULT_CACHE_MAX_ENTRIES
---
+++ backend/src/ferron/service.py
@@ -0,0 +1,55 @@
+from typing import Annotated
+
+from fastapi import Depends
+from loguru import logger
+from sqlmodel.ext.asyncio.session import AsyncSession
+from sqlalchemy import select
+
+from src.ferron import models, schemas, exceptions
+from src.database import get_session
+
+
+async def create_global_config(
+    global_config_data: schemas.GlobalConfig,
+    session: Annotated[AsyncSession, Depends(get_session)],
+) -> models.GlobalConfig:
+    try:
+        _existing_config = await read_global_config(session)
+    except exceptions.ConfigNotFound:
+        global_config = models.GlobalConfig(**global_config_data.model_dump(exclude_defaults=True))
+        session.add(global_config)
+        await session.commit()
+        await session.refresh(global_config)
+
+        return global_config
+    else:
+        logger.warning("GlobalConfig already exists. Creation skipped.")
+        raise exceptions.GlobalConfigAlreadyExists()
+
+async def update_global_config(
+        global_config_data: schemas.GlobalConfig,
+        session: Annotated[AsyncSession, Depends(get_session)]
+) -> models.GlobalConfig:
+    existing_config = await read_global_config(session)
+
+    update_data = global_config_data.model_dump(exclude_defaults=True)
+    for field, value  in update_data.items():
+        setattr(existing_config, field, value)
+
+    session.add(existing_config)
+    await session.commit()
+    await session.refresh(existing_config)
+
+    return existing_config
+
+async def read_global_config(
+        session: Annotated[AsyncSession, Depends(get_session)]
+) -> models.GlobalConfig | None:
+    statement = select(models.GlobalConfig).where(models.GlobalConfig.id == 1)
+
+    result = await session.exec(statement)
+    config = result.scalar_one_or_none()
+
+    if not config:
+        raise exceptions.ConfigNotFound(config_type="global configuration")
+    return config
