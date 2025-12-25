13ad01bc14c569100d23978f5ef7508c06fb3b7c
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-12-25 17:41:54 +0530

    refactor(auth/exceptions.py): update exception classes to have the same `detail` value structure as exceptions define in `ferron/exceptions.py`

--- backend/src/auth/exceptions.py
+++ backend/src/auth/exceptions.py
@@ -3,39 +3,51 @@ from fastapi import HTTPException, status
 
 class AuthException(HTTPException):
 
-    def __init__(self, detail: str, status_code: int = status.HTTP_400_BAD_REQUEST):
+    def __init__(self, detail: str | dict, status_code: int = status.HTTP_400_BAD_REQUEST):
         super().__init__(status_code=status_code, detail=detail)
 
 
 class InvalidCredentialsException(AuthException):
-    def __init__(self, detail: str = "Invalid username or password"):
+    def __init__(self, message: str = "Invalid credentials"):
         super().__init__(
-            detail=detail,
+            detail={
+                "error_code": "invalid_credentials",
+                "message": message
+            },
             status_code=status.HTTP_401_UNAUTHORIZED,
         )
         self.headers = {"WWW-Authenticate": "Bearer"}
 
 
 class UserAlreadyExistsException(AuthException):
-    def __init__(self, detail: str = "User already exists"):
+    def __init__(self, message: str = "User already exists"):
         super().__init__(
-            detail=detail,
+            detail={
+                "error_code": "user_already_exists",
+                "message": message
+            },
             status_code=status.HTTP_400_BAD_REQUEST,
         )
 
 
 class InvalidTokenException(AuthException):
-    def __init__(self, detail: str = "Could not validate token"):
+    def __init__(self, message: str = "Could not validate token"):
         super().__init__(
-            detail=detail,
+            detail={
+                "error_code": "invalid_token",
+                "message": message
+            },
             status_code=status.HTTP_401_UNAUTHORIZED,
         )
         self.headers = {"WWW-Authenticate": "Bearer"}
 
 
 class UserNotFoundException(AuthException):
-    def __init__(self, detail: str = "User not found"):
+    def __init__(self, message: str = "User not found"):
         super().__init__(
-            detail=detail,
+            detail={
+                "error_code": "user_not_found",
+                "message": message
+            },
             status_code=status.HTTP_404_NOT_FOUND,
         )
