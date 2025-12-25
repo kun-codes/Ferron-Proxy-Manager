861b7de80b516116945af45e3ee8ef93aaf689c5
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2025-12-25 18:01:59 +0530

    fix(ferron/router.py): add missing error responses for reverse proxy endpoints

--- backend/src/ferron/router.py
+++ backend/src/ferron/router.py
@@ -7,7 +7,7 @@ from src.auth.dependencies import get_current_user
 from src.database import get_session
 from src.ferron import service
 from src.ferron import schemas
-from src.ferron.exceptions import ConfigNotFound, GlobalConfigAlreadyExists
+from src.ferron.exceptions import ConfigNotFound, GlobalConfigAlreadyExists, VirtualHostNameAlreadyExists
 from src.utils import generate_error_response
 
 router = APIRouter(
@@ -48,7 +48,10 @@ async def update_global_config(
     config = await service.update_global_config(global_config_data, session)
     return config
 
-@router.post("/reverse-proxy")
+@router.post(
+    "/reverse-proxy",
+    responses=generate_error_response(VirtualHostNameAlreadyExists)
+)
 async def create_reverse_proxy_config(
         create_reverse_proxy_config_data: schemas.CreateReverseProxyConfig,
         session: Annotated[AsyncSession, Depends(get_session)]
@@ -56,15 +59,21 @@ async def create_reverse_proxy_config(
     config = await service.create_reverse_proxy_config(create_reverse_proxy_config_data, session)
     return config
 
-@router.patch("/reverse-proxy")
+@router.patch(
+    "/reverse-proxy",
+    responses=generate_error_response(ConfigNotFound)
+)
 async def update_reverse_proxy_config(
         update_reverse_proxy_config_data: schemas.UpdateReverseProxyConfig,
-        session: Annotated[AsyncSession, Depends(get_session)]
+        session: Annotated[AsyncSession, Depends(get_session)],
 ) -> schemas.UpdateReverseProxyConfig:
     config = await service.update_reverse_proxy_config(update_reverse_proxy_config_data, session)
     return config
 
-@router.get("/reverse-proxy")
+@router.get(
+    "/reverse-proxy",
+    responses=generate_error_response(ConfigNotFound, "reverse proxy configuration")
+)
 async def read_reverse_proxy_config(
         reverse_proxy_id: int,
         session: Annotated[AsyncSession, Depends(get_session)]
@@ -72,7 +81,10 @@ async def read_reverse_proxy_config(
     config = await service.read_reverse_proxy_config(reverse_proxy_id, session)
     return config
 
-@router.delete("/reverse-proxy")
+@router.delete(
+    "/reverse-proxy",
+    responses=generate_error_response(ConfigNotFound, "reverse proxy configuration")
+)
 async def delete_reverse_proxy_config(
         reverse_proxy_id: int,
         session: Annotated[AsyncSession, Depends(get_session)]
