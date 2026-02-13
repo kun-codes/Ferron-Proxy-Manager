380f59d14425f9beeb53ea68c3c679c6812fe310
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2026-02-14 01:51:17 +0530

    fix(frontend-components): update heading from "Manage Hosts" to "Manage Virtual Hosts" in `routes/dashboard/hosts/manage/+page.svelte`
    (cherry picked from commit 06ca86bab774c63a28f370cb4270684054908e36)

--- frontend/src/routes/dashboard/hosts/manage/+page.svelte
+++ frontend/src/routes/dashboard/hosts/manage/+page.svelte
@@ -5,7 +5,7 @@
 <div class="space-y-6">
     <div>
         <div class="mb-3">
-            <h1 class="text-3xl font-bold">Manage Hosts</h1>
+            <h1 class="text-3xl font-bold">Manage Virtual Hosts</h1>
         </div>
         <p class="text-sm text-muted-foreground">
             View, edit, and manage your virtual hosts and reverseproxies.
