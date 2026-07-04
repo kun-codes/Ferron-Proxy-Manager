9a5361d48369b7ec357d3b7f5c508560f9c1d273
Bishwa Saha <77796630+kun-codes@users.noreply.github.com>
2026-07-04 21:20:36 +0530

    add loggging

--- backend/src/fpm/utils.py
+++ backend/src/fpm/utils.py
@@ -199,10 +199,15 @@ async def _fetch_static_favicon(virtual_host_name: str, https_port: int, resolve
 
     async with httpx.AsyncClient(verify=False) as client:
         response = await client.get(target_url, headers=headers, extensions=extensions)
+        logger.info(f"fetched response for {target_url} with headers={headers} and extensions={extensions}")
         html_content = response.text
 
     root_url = f"https://{virtual_host_name}:{https_port}/"
+    logger.info(
+        f"about to fetch favicons for '{virtual_host_name}' from {root_url} with html_content={html_content!r}..."
+    )
     favicons = from_html(html_content, root_url=root_url, include_fallbacks=True)
+    logger.info(f"...got {len(favicons)} favicons for '{virtual_host_name}'")
 
     if favicons:
         favicon = favicons.pop()
